fada1d39f7bc4f4780930f4a29a033440728127f
Agustina Aperlo <43002670+magustinaperlo@users.noreply.github.com>
2022-09-25 16:59:03 -0300

    Update Ejerc2.3GenerNum.py
    Corrección en el código

--- Ejerc2.3GenerNum.py
+++ Ejerc2.3GenerNum.py
@@ -1,4 +1,3 @@
-
 from tkinter import*
 from random import*
 
@@ -13,6 +12,9 @@ def GenerarNumero():
 ventana=Tk()
 ventana.title("Generador de números")
 ventana.geometry('500x300')
+#fijamos los valores para que el usuario no extienda ni achique demasiado la ventana.
+ventana.minsize(500, 300) 
+ventana.maxsize(500, 300)
 ventana.config(bg="#BFACE0")
 
 num1=Label(ventana, text="Número 1", bg="#A084CA",borderwidth=5,font="arial,14")
@@ -23,9 +25,10 @@ num2.place(x=80,y=120)
 numvariable1=StringVar()
 numvariable2=StringVar()
 
-box1=Spinbox(ventana, from_=0,fg="#533483", to_= 5000, textvariable=numvariable1,borderwidth=10)
+#se añade el estado en el Spinbox para que el usuario NO ingrese valores manualmente y utilice el spinbox
+box1=Spinbox(ventana, from_=0,fg="#533483", to_= 5000, textvariable=numvariable1,borderwidth=10, state="readonly")
 box1.place(x=270,y=50)
-box2=Spinbox(ventana, from_=0,fg="#533483", to_= 5000, textvariable=numvariable2,borderwidth=10)
+box2=Spinbox(ventana, from_=0,fg="#533483", to_= 5000, textvariable=numvariable2,borderwidth=10,state="readonly")
 box2.place(x=270,y=120)
 
 EtiqNumGener=Label(ventana, text="Numero Generado", bg="#645CAA",borderwidth=8,font="arial,12")
